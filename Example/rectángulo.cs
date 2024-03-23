91fbc74e37d587be6358fdc4d02a177e8e292cc9
Piero Alessandro Aranciaga <111090272+Piero0w0@users.noreply.github.com>
2024-03-23 08:52:32 -0500

    Rectangulo
    Se agrego la base del Rectangulo

---
+++ Example/rectángulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarea2
+{
+    class Rectangulo
+    {
+        private int baseRectangulo;
+        private int alturaRectangulo;
+
+        public Rectangulo(int baseRectangulo, int alturaRectangulo)
+        {
+            this.baseRectangulo = baseRectangulo;
+            this.alturaRectangulo = alturaRectangulo;
+        }
+
+        {
+            return baseRectangulo * alturaRectangulo;
+        }
+    }
+}
